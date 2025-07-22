67985866966d6cd5ec12a9eed48e17d658906b3b
Usman Ali <160851064+usmanalixy@users.noreply.github.com>
2025-07-23 00:08:34 +0530

    Added Vercel Analytics component

--- deardocent-site/src/App.jsx
+++ deardocent-site/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -28,6 +29,9 @@ function App() {
       
       {/* Footer - Sticks to bottom */}
       <Footer />
+      
+      {/* Vercel Analytics - Add at the bottom */}
+      <Analytics />
     </div>
   );
 }
